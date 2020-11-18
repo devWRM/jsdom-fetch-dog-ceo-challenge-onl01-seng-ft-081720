ba8132dd1f4cf20cc8afb3be29565d93c3e220c7
devWRM <mcwen@comcast.net>
2020-11-18 18:33:53 -0500

    commit tree c33a5f53d7b0cb2f209592ef2549e3dd8c35a39d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+
+  // BELOW Also works
+  // return fetch('https://anapioficeandfire.com/api/books')
+  // .then(resp => resp.json())
+  // .then(json => renderBooks(json));
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+})
+  
+
